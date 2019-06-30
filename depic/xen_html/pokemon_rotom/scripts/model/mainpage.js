f0abb4c4da4ade52a63169fc7cabc7c4373cc716
marcgrey <geraldnalzaga@gmail.com>
2019-06-30 23:41:05 +0800

    Added Rotom

---
+++ depic/xen_html/pokemon_rotom/scripts/model/mainpage.js
@@ -0,0 +1,15 @@
+var modelData = [
+  ['1',
+  'Rotom',
+  'u/marcusenzo',
+  'Xen HTML',
+  'depic/xen_html/pokemon_rotom/images/001.png',
+  'depic/xen_html/pokemon_rotom/images/002.png',
+  'rgba(238,114,42,1)',
+  'depic/xen_html/pokemon_rotom/index.html',
+  'Animated Rotom lockscreen widget. It\'s life represents the battery life of your mobile device.']
+];
+
+var paypalLabel = 'Donate';
+
+var paypalMeLink = 'https://www.paypal.me/geraldalzaga'
