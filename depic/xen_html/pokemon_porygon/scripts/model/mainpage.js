9ceeafe81592261a2a25d48662d8e3fa21e13759
marcgrey <geraldnalzaga@gmail.com>
2019-06-30 21:00:38 +0800

    Added Porygon

---
+++ depic/xen_html/pokemon_porygon/scripts/model/mainpage.js
@@ -0,0 +1,15 @@
+var modelData = [
+  ['1',
+  'Porygon',
+  'u/marcusenzo',
+  'Xen HTML',
+  'depic/xen_html/pokemon_porygon/images/001.png',
+  'depic/xen_html/pokemon_porygon/images/002.png',
+  'rgba(253,116,132,1)',
+  'depic/xen_html/pokemon_porygon/index.html',
+  'Animated Porygon lockscreen widget. It\'s life represents the battery life of your mobile device.']
+];
+
+var paypalLabel = 'Donate';
+
+var paypalMeLink = 'https://www.paypal.me/geraldalzaga'
